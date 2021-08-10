e9bb85d0403d7b722ff159ff0d31bef7d3e18d19
Corina <desarrollocso@gmail.com>
2021-08-10 22:17:39 +0200

    Creado el controlador,servicio,repository

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,47 +1,65 @@
 package com.example.demo;
 
 import com.example.demo.contacto.Contacto;
+import com.example.demo.contacto.ContactoRepository;
 import com.example.demo.contacto.Genero;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
-	final private static List<Contacto> contactos=new ArrayList<>();
-
 	public static void main(String[] args) {
-		crearArrayContactos();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	public static void crearArrayContactos(){
-		int contador = 0;
-		for (int i = 0; i < 10; i++) {
-			contador++;
-			Long ident= Long.parseLong(String.valueOf(contador));
-			String email = "contacto" + (contador)+ "@gmail.com";
-			String nombre = "Contacto"+ (contador);
-			Random aleatorio = new Random();
-			Contacto contacto = new Contacto(
-					ident,
-					nombre,
-					LocalDate.of(aleatorio.nextInt(10)+1970,aleatorio.nextInt(12)+1,aleatorio.nextInt(30)+1),
-					email,
-					Genero.Hombre);
-			contactos.add(contacto);
-		}
+	@Bean
+	CommandLineRunner runner(ContactoRepository contactoRepository, MongoTemplate mongoTemplate){
+		return args -> {
+			int contador = 0;
+			for (int i = 0; i < 10; i++) {
+				contador++;
+				String email = "contacto" + (contador)+ "@gmail.com";
+				String nombre = "Contacto"+ (contador);
+				Random aleatorio = new Random();
+				Contacto contacto = new Contacto(
+						nombre,
+						LocalDate.of(aleatorio.nextInt(10)+1970,aleatorio.nextInt(12)+1,aleatorio.nextInt(30)+1),
+						email,
+						Genero.Hombre);
+
+//				usandoMongoTemplateAndQuery(contactoRepository, mongoTemplate, email, contacto);
+				contactoRepository.findContactoByEmail(email)
+						.ifPresentOrElse(c -> System.out.println(c + " ya existe "),()->{
+							System.out.println("Contacto insertado correctamente ->"+ contacto);
+							contactoRepository.insert(contacto);
+						});
+			}
+		};
 	}
-	@GetMapping
-	public List<Contacto> listaContactos(){
-		return contactos;
+
+	private void usandoMongoTemplateAndQuery(ContactoRepository contactoRepository, MongoTemplate mongoTemplate, String email, Contacto contacto) {
+		Query query=new Query();
+		query.addCriteria(Criteria.where("email").is(email));
+
+		List<Contacto> contactos = mongoTemplate.find(query, Contacto.class);
+		if (contactos.size()>1){
+			throw new IllegalStateException("Ya existe un contacto con dicho email " + email);
+		}
+		if (contactos.isEmpty()) {
+			System.out.println("Contacto insertado correctamente ->"+ contacto);
+			contactoRepository.insert(contacto);
+		} else {
+			System.out.println(contacto + " ya existe ");
+		}
 	}
 }
--- src/main/java/com/example/demo/contacto/Contacto.java
+++ src/main/java/com/example/demo/contacto/Contacto.java
@@ -1,12 +1,18 @@
 package com.example.demo.contacto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDate;
 import java.time.Period;
-
+@Document
 public class Contacto {
-    private Long id;
+    @Id
+    private String id;
     private String nombre;
     private LocalDate fechaNacimiento;
+    @Indexed(unique = true)
     private String email;
     private Genero genero;
 
@@ -17,25 +23,11 @@ public class Contacto {
         this.genero = genero;
     }
 
-    public Contacto(Long id, String nombre, LocalDate fechaNacimiento, String email, Genero genero) {
-        this.id=id;
-        this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        this.email = email;
-        this.genero = genero;
-    }
-
-    public Contacto(Long id,String nombre, String email) {
-        this.id=id;
-        this.nombre = nombre;
-        this.email = email;
-    }
-
     public int getEdad() {
         return Period.between(this.fechaNacimiento, LocalDate.now()).getYears();
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
